803d2df41a5dcc5efc617d39d02326c627cd789b
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-06-21 20:20:46 -0300

    criando componentes e passando por props

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,7 +1,22 @@
 import React from "react"
 
+const BemVindo = (props) => {
+  return(
+    <div>
+      <h2>Bem-vindo(a) {props.nome}</h2>
+      <h3>Tenho {props.idade} anos</h3>
+    </div>
+  )
+}
+
 function App(){
-  return <h1>Olá mundo!</h1>
+  return(
+    <div>
+      <h1>Olá mundo!</h1>
+      <BemVindo nome='Gabriel' idade='20'/>
+      <BemVindo nome='Malu' idade='17'/>
+    </div>
+  ) 
 }
 
 export default App
