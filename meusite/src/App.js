5a09f36163df7df1f47af0b9b68c09eda722dd12
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-06-23 18:48:23 -0300

    eventos no react js

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,31 +1,11 @@
 import React, { Component } from 'react'
+import Membro from './components/Membro'
 
 class App extends Component{
-
-  constructor(props){
-    super(props)
-    this.state = {
-      hora: '00:00:00'
-    }
-  }
-
-  componentDidMount(){
-
-    setInterval(() => {
-      this.setState({ hora: new Date().toLocaleTimeString() })
-    }, 1000)
-
-  }
-
-  componentDidUpdate(){
-    console.log('BOAAA')
-  }
-
-
   render(){
     return(
       <div>
-        <h1>Meu Projeto {this.state.hora} </h1>
+        <Membro nome='Visitante' />
       </div>
     )
   }
