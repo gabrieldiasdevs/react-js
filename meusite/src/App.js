472c7ed989936a0a66472fb8d2da438446e2a181
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-06-24 19:56:38 -0300

    trabalhando com listas no react

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,38 +1,54 @@
 import React, { Component } from 'react'
+import Feed from './components/Feed'
+
 class App extends Component{
 
   constructor(props){
     super(props)
     this.state = {
-      status: false
+      feed: [
+      {
+        id: 1,
+        username: 'Gabriel',
+        curtidas: 10,
+        comentarios: 2
+      },
+      {
+        id: 2,
+        username: 'Malu',
+        curtidas: 2984,
+        comentarios: 124
+      },
+      {
+        id: 3,
+        username: 'Sabrina',
+        curtidas: 996,
+        comentarios: 56
+      },
+      {
+        id: 4,
+        username: 'Rafael',
+        curtidas: 1,
+        comentarios: 0
+      }
+    ]
     }
-
-    this.entrar = this.entrar.bind(this)
-    this.sair = this.sair.bind(this)
-  }
-
-  entrar(){
-    this.setState({ status: true })
-  }
-
-  sair(){
-    this.setState({ status: false })
   }
 
   render(){
     return(
       <div>
         
-        {this.state.status ?
-          <div>
-            <h2>Olá visitante</h2>
-            <button onClick={this.entrar}>Entrar no Sistema</button>
-          </div> :
-          <div>
-            <h2>Bem vindo ao sistema</h2>
-            <button onClick={this.sair}>Sair</button>
-          </div>
-        }
+        {this.state.feed.map((item) => {
+          return(
+            <Feed 
+              key={item.id} 
+              username={item.username} 
+              curtidas={item.curtidas}
+              comentarios={item.comentarios}
+            />
+          )
+        })}
 
       </div>
     )
