9b4f170350bb194216d913ba277342419d20faa9
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-06-27 23:12:30 -0300

    projeto cronometro finalizado

--- cronometro/src/App.js
+++ cronometro/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import './style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props)
+    this.state = {
+      numero: 0,
+      botao: 'INICIAR'
+    }
+    this.timer = null
+    this.iniciar = this.iniciar.bind(this)
+    this.limpar = this.limpar.bind(this)
+  }
+
+  iniciar(){
+    if(this.timer !== null){
+      clearInterval(this.timer)
+      this.timer = null
+      this.setState({ botao: 'INICIAR' })
+    }else{
+      this.timer = setInterval(() => {
+        let state = this.state
+        state.numero += 0.1
+        this.setState(state)
+      }, 100);
+      this.setState({ botao: 'PAUSAR' })
+    }    
+  }
+
+  limpar(){
+    if(this.timer !== null){
+      clearInterval(this.timer)
+      this.timer = null
+    }
+
+    this.setState({ numero: 0 })
+    this.setState({ botao: 'INICIAR' })
+  }
+
+  render(){
+    return(
+      <div className="container" >
+        <img src={require('./assets/cronometro.png')} className="img" />
+        <a className="timer" >{this.state.numero.toFixed(1)}</a>
+
+        <div className="areaBtn">
+          <a onClick={this.iniciar} className="botao" >{this.state.botao}</a>
+          <a onClick={this.limpar} className="botao" >LIMPAR</a>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
