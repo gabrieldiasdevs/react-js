20751f8c4f71883f6ab4f97bd3c23e2e2034576e
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-06-28 21:26:35 -0300

    requisições com fetch

--- requisicoes-http/src/App.js
+++ requisicoes-http/src/App.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './style.css'
 
 function App() {
+  const [nutri, setNutri] = useState([])
+
+  useEffect(() => {
+
+    function loadApi(){
+      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts'
+      fetch(url)
+      .then((r) => r.json())
+      .then((json) => {
+        setNutri(json)
+      })
+    }
+
+    loadApi()
+
+  }, [])
+
   return (
-    <div>
-      <h1>TESTE</h1>
+    <div className='container' >
+      <header>
+        <strong>React Nutri</strong>
+      </header>
+
+      {nutri.map((item) => {
+        return(
+          <article key={item.id} className='post' >
+            <strong className='titulo' >{item.titulo}</strong>
+            <img src={item.capa} alt={item.titulo} className='capa' />
+            <p className='subtitulo' >{item.subtitulo}</p>
+            <a className='botao' >Acessar</a>
+          </article> 
+        )
+      })}
     </div>
   )
 }
