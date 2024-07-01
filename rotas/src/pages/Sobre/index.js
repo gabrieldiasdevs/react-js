5732f3cc1682acd59945d94cca181c5291622900
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-07-01 16:41:52 -0300

    trabalhando com rotas em react js

---
+++ rotas/src/pages/Sobre/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sobre() {
+  return (
+    <div>
+      <h1>SOBRE</h1>
+    </div>
+  )
+}
+
+export default Sobre
---
+++ rotas/src/routes.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Sobre from './pages/Sobre'
+
+function RoutesApp(){
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={ <Home/> }
+        />
+
+        <Route
+          path='/sobre'
+          element={ <Sobre/> }
+        />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default RoutesApp
