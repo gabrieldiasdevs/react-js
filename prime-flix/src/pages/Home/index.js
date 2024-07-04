5c6743d76725eca68f30a07fb68bcfcbee2603f2
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-07-03 21:23:58 -0300

    tela de carregamento e erro 404 finalizada

--- prime-flix/src/pages/Home/index.js
+++ prime-flix/src/pages/Home/index.js
@@ -6,6 +6,7 @@ import './home.css'
 
 function Home(){
   const [filmes, setFilmes] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
 
@@ -20,11 +21,20 @@ function Home(){
 
       setFilmes(response.data.results.slice(0, 10))
 
+      setLoading(false)
     }
 
     loadFilmes()
 
   }, [])
+
+  if(loading){
+    return(
+      <div className='loading' >
+        <h2>Carregando filmes...</h2>
+      </div>
+    )
+  }
 
   return(
     <div className='container' >
--- prime-flix/src/routes.js
+++ prime-flix/src/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Filme from './pages/Filme'
+import Erro from './pages/Erro'
 
 import Header from './components/Header'
 
@@ -13,6 +14,8 @@ function RoutesApp(){
 
         <Route path='/' element={ <Home/> } />
         <Route path='/filme/:id' element={ <Filme/> } />
+
+        <Route path='*' element={ <Erro/> } />
 
       </Routes>
     </BrowserRouter>
