af96d2b0c0b01ef70fc616ff2a16ba0c680b3f98
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-07-04 21:48:57 -0300

    função trailer do filme finalizada

--- prime-flix/src/pages/Filme/index.js
+++ prime-flix/src/pages/Filme/index.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 
 import api from '../../services/api'
 
 import './filme-info.css'
 
 function Filme(){
+  const navigate = useNavigate()
+  
   const { id } = useParams()
   const [filme, setFilme] = useState({})
   const [loading, setLoading] = useState(true)
@@ -26,7 +28,8 @@ function Filme(){
 
       })
       .catch((err) => {
-        console.log(err)
+        navigate('/', { replace: true })
+        return
       })
     }
 
@@ -36,7 +39,7 @@ function Filme(){
       console.log('Componente Desmontado')
     }
  
-  }, [])
+  }, [navigate, id])
 
   if(loading){
     return(
@@ -58,7 +61,7 @@ function Filme(){
       <div className='area-buttons'>
         <button>Salvar</button>
         <button>
-          <a href='#' >
+          <a href={`https://youtube.com/results?search_query=${filme.title} Trailer`} target='_blank' rel='external' >
             Trailer
           </a>
         </button>
