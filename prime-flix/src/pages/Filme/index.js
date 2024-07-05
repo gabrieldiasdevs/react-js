92ce58381b16127cd59f986fec10caa1c08ef8de
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-07-04 22:21:20 -0300

    criando  a função de favoritar filme

--- prime-flix/src/pages/Filme/index.js
+++ prime-flix/src/pages/Filme/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { useParams, useNavigate } from 'react-router-dom'
+import { useParams, useNavigate, json } from 'react-router-dom'
 
 import api from '../../services/api'
 
@@ -41,6 +41,23 @@ function Filme(){
  
   }, [navigate, id])
 
+  function salvarFilme(){
+    const minhaLista = localStorage.getItem('@primeflix')
+
+    let filmesSalvos = JSON.parse(minhaLista) || []
+
+    const hasFilme = filmesSalvos.some((filmesSalvo) => filmesSalvo.id === filme.id)
+
+    if(hasFilme){
+      alert('Esse filme já está na lista!')
+      return
+    }
+
+    filmesSalvos.push(filme)
+    localStorage.setItem('@primeflix', JSON.stringify(filmesSalvos))
+    alert('Filme salvo com sucesso!')
+  }
+
   if(loading){
     return(
       <div className='filme-info ' >
@@ -59,9 +76,9 @@ function Filme(){
       <strong>Avaliação: {filme.vote_average.toFixed(1)} / 10</strong>
 
       <div className='area-buttons'>
-        <button>Salvar</button>
+        <button onClick={salvarFilme} >Salvar</button>
         <button>
-          <a href={`https://youtube.com/results?search_query=${filme.title} Trailer`} target='_blank' rel='external' >
+          <a href={`https://youtube.com/results?search_query=${filme.title} Trailer`} target='blank' rel='external' >
             Trailer
           </a>
         </button>
