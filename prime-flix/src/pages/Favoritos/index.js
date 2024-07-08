37e049c59bcf0f88a511d24c5cc6fa2be85e84a3
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-07-07 22:41:33 -0300

    criando funcionalidade de excluir filme favorito

--- prime-flix/src/pages/Favoritos/index.js
+++ prime-flix/src/pages/Favoritos/index.js
@@ -1,9 +1,34 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import './favoritos.css'
 
 function Favoritos(){
+  const [filmes, setFilmes] = useState([])
+
+  useEffect(() => {
+
+    const minhaLista = localStorage.getItem('@primeflix')
+    setFilmes(JSON.parse(minhaLista) || [])
+
+  }, [])
+
   return(
-    <div>
-      <h1>Tela Favoritos</h1>
+    <div className='meus-filmes' >
+      <h1>Meus Filmes</h1>
+
+      <ul>
+        {filmes.map((item) => {
+          return(
+            <li key={item.id}>
+              <span>{item.title}</span>
+              <div>
+                <Link to={`/filme/${item.id}`} >Ver Detalhes</Link>
+                <button>Excluir</button>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
