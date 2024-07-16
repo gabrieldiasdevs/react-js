8750a8f2a13be01ed1273e59f3c5cc498411367e
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-07-15 22:18:16 -0300

    criando tela admin

--- lista-de-tarefas/src/pages/Home/index.js
+++ lista-de-tarefas/src/pages/Home/index.js
@@ -1,9 +1,25 @@
 import { useState, useEffect } from 'react'
+import { addDoc } from 'firebase/firestore'
+import { db } from '../../services/firebaseConnection'
+
+import './home.css'
 
 function Home(){
+  const [tarefa, setTarefa] = useState('')
+
+  async function handleRegisterTask(){
+    
+  }
+
   return(
-    <div>
-      <span>LOGIN</span>
+    <div className='container-home' >
+      <h1>Minhas Tarefas</h1>
+      <input
+        placeholder='Digite sua tarefa...'
+        value={tarefa}
+        onChange={(e) => setTarefa(e.target.value)}
+      />
+      <button onClick={handleRegisterTask} >Registrar tarefa</button>
     </div>
   )
 }
