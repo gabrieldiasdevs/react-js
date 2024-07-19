ffc5c83880f0d4865c07515592fe86724a654fd4
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-07-19 13:07:34 -0300

    função permanecer usuário logado finalizado

--- lista-de-tarefas/src/routes/Private.js
+++ lista-de-tarefas/src/routes/Private.js
@@ -3,13 +3,44 @@ import { useState, useEffect } from 'react'
 import { auth } from '../services/firebaseConnection'
 import { onAuthStateChanged } from 'firebase/auth'
 
+import { Navigate } from 'react-router-dom'
+
 function Private({ children }){
   const [loading, setLoading] = useState(true)
   const [signed, setSigned] = useState(false)
 
   useEffect(() => {
+    async function checkLogin(){
+      const unsub = onAuthStateChanged(auth, (user) => {
+        if(user){
+          const userData = {
+            uid: user.uid,
+            email: user.email
+          }
 
+          localStorage.setItem('@detailUser', JSON.stringify(userData))
+
+          setLoading(false)
+          setSigned(true)
+        }else{
+          setLoading(false)
+          setSigned(false)
+        }
+      })
+    }
+
+    checkLogin()
   }, [])
+
+  if(loading){
+    return(
+      <div></div>
+    )
+  }
+
+  if(!signed){
+    return <Navigate to='/' />
+  }
 
   return children
 }
