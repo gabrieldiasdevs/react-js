845b2207ebf86b4c1c7f1bc79d27cf4679a6c50a
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-07-23 22:10:16 -0300

    funcionalidade de login de usuário finalizada

--- chamados/src/contexts/auth.js
+++ chamados/src/contexts/auth.js
@@ -1,6 +1,6 @@
 import { useState, createContext, useEffect } from 'react'
 import { auth, db  } from '../services/firebaseConnection'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 import { doc, getDoc, setDoc } from 'firebase/firestore'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
@@ -12,8 +12,34 @@ function AuthProvider({ children }){
   const [user, setUser] = useState(null)
   const [loadingAuth, setLoadingAuth] = useState(false)
 
-  function signIn(email, password){
+  async function signIn(email, password){
+    setLoadingAuth(true)
 
+    await signInWithEmailAndPassword(auth, email, password)
+    .then( async (value) => {
+      let uid = value.user.uid
+
+      const docRef = doc(db, 'users', uid)
+      const docSnap = await getDoc(docRef)
+
+      let data = {
+        uid: uid,
+        nome: docSnap.data().nome,
+        email: value.user.email,
+        avatarUrl: docSnap.data().avatarUrl
+      }
+
+      setUser(data)
+      storageUser(data)
+      setLoadingAuth(false)
+      toast.success('Bem-vindo(a) de volta!')
+      navigate('/dashboard')
+    })
+    .catch((error) => {
+      console.log(error)
+      setLoadingAuth(false)
+      toast.error('Ops algo deu errado!')
+    })
   }
 
   async function signUp(email, password, name){
@@ -38,7 +64,7 @@ function AuthProvider({ children }){
         setUser(data)
         storageUser(data)
         setLoadingAuth(false)
-        toast.success('Seja bem-vindo ao sistema!')
+        toast.success('Seja bem-vindo(a) ao sistema!')
         navigate('/dashboard')
       })
       .catch((error) => {
@@ -49,6 +75,7 @@ function AuthProvider({ children }){
     .catch((error) => {
       console.log(error)
       setLoadingAuth(false)
+      toast.error('Ops algo deu errado!')
     })
   }
 
