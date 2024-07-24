f2938518dfc7c764aa67b79a42da6ddef069bec5
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-07-23 21:31:20 -0300

    funcionalidade de cadastro de usuário finalizada

--- chamados/src/contexts/auth.js
+++ chamados/src/contexts/auth.js
@@ -2,10 +2,13 @@ import { useState, createContext, useEffect } from 'react'
 import { auth, db  } from '../services/firebaseConnection'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { doc, getDoc, setDoc } from 'firebase/firestore'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 export const AuthContext = createContext({})
 
 function AuthProvider({ children }){
+  const navigate = useNavigate()
   const [user, setUser] = useState(null)
   const [loadingAuth, setLoadingAuth] = useState(false)
 
@@ -33,7 +36,10 @@ function AuthProvider({ children }){
         }
 
         setUser(data)
+        storageUser(data)
         setLoadingAuth(false)
+        toast.success('Seja bem-vindo ao sistema!')
+        navigate('/dashboard')
       })
       .catch((error) => {
         console.log(error)
@@ -44,6 +50,10 @@ function AuthProvider({ children }){
       console.log(error)
       setLoadingAuth(false)
     })
+  }
+
+  function storageUser(data){
+    localStorage.setItem('@ticketsPRO', JSON.stringify(data))
   }
 
   return(
