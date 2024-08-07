355823d0ad69bbece7c84999be024a14804a7703
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-08-07 20:18:02 -0300

    componente title criado

---
+++ chamados/src/pages/Profile/index.js
@@ -0,0 +1,21 @@
+import Header from '../../components/Header'
+import Title from '../../components/Title'
+
+import { FiSettings } from 'react-icons/fi'
+
+import './profile.css'
+
+export default function Profile(){
+  return(
+    <div>
+      <Header/>
+
+      <div className='content' >
+        <Title name='Minha conta' >
+          <FiSettings size={25} />
+        </Title>
+      </div>
+      <h1>Página Perfil</h1>
+    </div>
+  )
+}
