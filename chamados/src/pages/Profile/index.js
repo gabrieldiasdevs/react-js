db73cd9d3bd6e53e247058bda3044b121a3c37d7
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-08-20 22:30:55 -0300

    atualizando perfil do usuário (foto e nome)

--- chamados/src/pages/Profile/index.js
+++ chamados/src/pages/Profile/index.js
@@ -6,6 +6,12 @@ import { FiSettings, FiUpload } from 'react-icons/fi'
 import avatar from '../../assets/avatar.png'
 import { AuthContext } from '../../contexts/auth'
 
+import { doc, updateDoc } from 'firebase/firestore'
+import { db, storage } from '../../services/firebaseConnection'
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
+
+import { toast } from 'react-toastify'
+
 import './profile.css'
 
 export default function Profile(){
@@ -32,6 +38,58 @@ export default function Profile(){
     }
   }
 
+  async function handleUpload(){
+    const currentUid = user.uid
+    const uploadRef = ref(storage, `images/${currentUid}/${imageAvatar.name}`)
+
+    const uploadTask = uploadBytes(uploadRef, imageAvatar)
+    .then((snapshot) => {
+      getDownloadURL(snapshot.ref).then( async (downloadUrl) => {
+        let urlFoto = downloadUrl
+
+        const docRef = doc(db, 'users', user.uid)
+        await updateDoc(docRef, {
+          avatarUrl: urlFoto,
+          nome: nome
+        })
+        .then(() => {
+          let data = {
+            ...user,
+            nome: nome,
+            avatarUrl: urlFoto,
+          }
+  
+          setUser(data)
+          storageUser(data)
+          toast.success('Atualizado com sucesso!')
+        })
+      })
+    })
+  }
+
+  async function handleSubmit(e){
+    e.preventDefault()
+
+    if(imageAvatar === null && nome !== ''){
+      const docRef = doc(db, "users", user.uid)
+      await updateDoc(docRef, {
+        nome: nome
+      })
+      .then(() => {
+        let data = {
+          ...user,
+          nome: nome
+        }
+
+        setUser(data)
+        storageUser(data)
+        toast.success('Atualizado com sucesso!')
+      })
+    }else if(nome !== '' && imageAvatar !== null){
+      handleUpload()
+    }
+  }
+
   return(
     <div>
       <Header/>
@@ -43,7 +101,7 @@ export default function Profile(){
 
         <div className='container' >
 
-          <form className='form-profile' >
+          <form className='form-profile' onSubmit={handleSubmit} >
             <label className='label-avatar' >
               <span>
                 <FiUpload color='#FFF' size={25}/>
