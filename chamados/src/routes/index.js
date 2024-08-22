0b865293d34ec8901a4eb408ceb3a71a05467ceb
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-08-21 23:11:01 -0300

    criando área de cadastro de clientes

--- chamados/src/routes/index.js
+++ chamados/src/routes/index.js
@@ -4,6 +4,7 @@ import SignIn from '../pages/SignIn'
 import SignUp from '../pages/SignUp'
 import Dashboard from '../pages/Dashboard'
 import Profile from '../pages/Profile'
+import Customers from '../pages/Customers'
 
 import Private from './Private'
 
@@ -15,6 +16,7 @@ export default function RoutesApp(){
 
       <Route path='/dashboard' element={ <Private> <Dashboard/> </Private> } />
       <Route path='/profile' element={ <Private> <Profile/> </Private> } />
+      <Route path='/customers' element={ <Private> <Customers/> </Private> } />
     </Routes>
   )
 }
