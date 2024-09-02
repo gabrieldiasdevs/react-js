f7b44298b077640f82614b5e06ee6f22a7867908
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-09-02 19:58:37 -0300

    desenvolvendo página dashboard

--- chamados/src/pages/Dashboard/index.js
+++ chamados/src/pages/Dashboard/index.js
@@ -2,6 +2,12 @@ import { useContext } from 'react'
 import { AuthContext } from '../../contexts/auth'
 
 import Header from '../../components/Header'
+import Title from '../../components/Title'
+
+import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+
+import './dashboard.css'
 
 export default function Dashboard(){
   const { logout } = useContext(AuthContext)
@@ -14,8 +20,47 @@ export default function Dashboard(){
     <div>
       <Header/>
 
-      <h1>TELA DASHBOARD</h1>
-      <button onClick={handleLogout} >Sair da conta</button>
+      <div className='content'>
+        <Title name='Tickets'>
+          <FiMessageSquare size={25} />
+        </Title>
+
+        <>
+          <Link to='/new' className='new' >
+            <FiPlus size={25} color='#FFF' />
+            Novo chamado
+          </Link>
+
+          <table>
+            <thead>
+              <tr>
+                <th scope='col' >Cliente</th>
+                <th scope='col' >Assunto</th>
+                <th scope='col' >Status</th>
+                <th scope='col' >Cadastrado em</th>
+                <th scope='col' >#</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td data-label='Cliente' >Mercado Matias</td>
+                <td data-label='Assunto' >Suporte</td>
+                <td data-label='Status' >Em Aberto</td>
+                <td data-label='Cadastrado' >12/05/2024</td>
+                <td data-label='#' >
+                  <button style={{ backgroundColor: '#3583f6' }} >
+                    <FiSearch color='#FFF' size={17} />
+                  </button>
+                  <button style={{ backgroundColor: '#f6a935' }} >
+                    <FiEdit2 color='#FFF' size={17} />
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </>
+      </div>
     </div>
   )
 }
