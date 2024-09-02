feb787106da55dab6ee602c39e51d831c117b86b
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-09-02 20:28:07 -0300

    tabela de tickets finalizada

--- chamados/src/pages/Dashboard/index.js
+++ chamados/src/pages/Dashboard/index.js
@@ -46,13 +46,17 @@ export default function Dashboard(){
               <tr>
                 <td data-label='Cliente' >Mercado Matias</td>
                 <td data-label='Assunto' >Suporte</td>
-                <td data-label='Status' >Em Aberto</td>
+                <td data-label='Status' >
+                  <span className='badge' style={{ backgroundColor: '#999' }} >
+                    Em Aberto
+                  </span>
+                </td>
                 <td data-label='Cadastrado' >12/05/2024</td>
                 <td data-label='#' >
-                  <button style={{ backgroundColor: '#3583f6' }} >
+                  <button className='action' style={{ backgroundColor: '#3583f6' }} >
                     <FiSearch color='#FFF' size={17} />
                   </button>
-                  <button style={{ backgroundColor: '#f6a935' }} >
+                  <button className='action' style={{ backgroundColor: '#f6a935' }} >
                     <FiEdit2 color='#FFF' size={17} />
                   </button>
                 </td>
